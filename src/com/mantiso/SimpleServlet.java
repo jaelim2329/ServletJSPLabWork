2d020e67b6e02881684241650a6cb757cf8c756e
jaelim <jaelim@visa.com>
2016-07-19 16:26:22 -0500

    Datetime servlet

---
+++ src/com/mantiso/SimpleServlet.java
@@ -0,0 +1,21 @@
+package com.mantiso;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/home", "*.do"})
+public class SimpleServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write(LocalDateTime.now().toString());
+	}
+}

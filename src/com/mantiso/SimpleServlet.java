e8720d14568a796ca23cec7e3a0efa9a694eab3b
jaelim <jaelim@visa.com>
2016-07-20 14:50:45 -0500

    Login + Cookie

--- src/com/mantiso/SimpleServlet.java
+++ src/com/mantiso/SimpleServlet.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,9 +14,50 @@ import javax.servlet.http.HttpServletResponse;
 public class SimpleServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private static final String USERNAME = "admin";
+	private static final String PASSWORD = "password";
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String name = req.getParameter("name");
+		
+		if (name != null){
+			resp.getWriter().printf("Name: %s\n", name);
+		} 
+		else {
+			resp.getWriter().write("Please enter a name.");
+		}
+		
 		resp.getWriter().write(LocalDateTime.now().toString());
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//Set response content type
+		resp.setContentType("text/html");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		if (username.equals(USERNAME) && password.equals(PASSWORD)) {
+			//Create cookie for username
+			Cookie user = new Cookie("usernameCookie", req.getParameter("username"));
+			
+			//Set expiration date for cookie
+			user.setMaxAge(60*60*24);
+			
+			//Add to response header
+			resp.addCookie(user);
+			
+			resp.getWriter().printf("Welcome %s", username);
+			
+			System.out.println(user.getName());
+			System.out.println(user.getValue());
+		}
+		else {
+			resp.getWriter().println("Bad Login ");
+			resp.getWriter().println("<a href=\"index.jsp\">Back</a>");
+		}
+		
 	}
 }
